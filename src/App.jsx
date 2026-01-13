ede6021888a20f6d376c57bb4714f3c5e7b333fa
junaid-sarwar <junaidsarwariqbal@gmail.com>
2026-01-14 01:24:09 +0500

    final final filhaal

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import { AuthProvider } from './context/AuthContext';
 import { Toaster } from "@/components/ui/sonner";
 
